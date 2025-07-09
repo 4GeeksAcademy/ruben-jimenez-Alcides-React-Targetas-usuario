2be0c4b50cc407d9d2c3fb4abfd90898a64e947e
RUB3N02 <rubyerizo@gmail.com>
2025-07-09 19:21:49 +0000

    falta la segunda tarjeta

---
+++ src/js/components/Formulario.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+
+export const Formulario = ({agregarContacto}) => {
+
+
+    const [email, setEmail] = useState("")
+    const [telefono, setTelefono] = useState("")
+    const [password, setPassword] = useState("")
+
+    // const [lista, setLista]= useState([])
+
+
+    const handleSubmit = (crear) => {
+        crear.preventDefault();
+        if(email===""|| telefono==="" || password=== "") return
+        const nuevoRegistro = { email, telefono, password }
+        agregarContacto(nuevoRegistro)
+        
+
+        // console.log("Guardado ✅:",nuevoRegistro);
+        setEmail("");
+        setTelefono("");
+        setPassword("");
+    };
+    // console.log("Lista Completa:", lista);
+
+    return (
+        <>
+            <form onSubmit={handleSubmit} >
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="Form-label">Email address</label>
+                    <input className="Form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                        type="email"
+                        value={email}
+                        onChange= {(loEscrito) => setEmail(loEscrito.target.value)} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleNumber1" className="Form-label" >Telefono</label>
+                    <input className="Form-control" id="exampleNumber1"
+                        type="telefono"
+                        value={telefono}
+                        onChange={loEscrito => setTelefono(loEscrito.target.value)} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="Form-label" >Password</label>
+                    <input  className="Form-control" id="exampleInputPassword1"
+                        type="password"
+                        value={password}
+                        onChange={loEscrito => setPassword(loEscrito.target.value)} />
+                </div>
+                <button type="submit" className="btn btn-primary" >Crear </button>
+            </form>
+        </>
+
+    )
+}
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Formulario } from "./Formulario";
 
-//create your first component
+// //create your first component
+// const Home = () => {
+//	 const [contador,setContador] = useState(0)
+// const sumaContador= ()=> setContador(contador + 1)
+// const restaContador= ()=> setContador(contador + 1)
+// 	return (
+// 		<>
+// 		<h3>{contador}</h3>
+// 		<button onClick={sumaContador}>+1</button>
+// 		<button onClick={restaContador}>-1</button>
+// 		</>
+// 	);
+// };
+// export default Home;
+
+// const Home = () => {
+// 	const[color, setColor] = useState('')
+// 	return (
+// 		<>
+// 		<h3 className={`text-${color}`}>No me queda</h3>
+// 		<button onClick={()=>setColor("primary")}>color1</button>
+// 		<button onClick={()=>setColor("warning")}>color2</button>
+// 		<button onClick={()=>setColor("success")}>color3</button>
+// 		<button onClick={()=>setColor("secondary")}>color4</button>
+// 		</>
+// 	);
+// };
+// export default Home;
+// const Home = () => {
+
+// 	const[frase, setFrase] = useState(true)
+// 	return (
+// 		<>
+// 		<h3 >{frase ? "Estoy visible" : " "}</h3>
+// 		<button onClick={()=>setFrase(!frase)}>{frase ? "Ocultar": "Mostrar "}</button>
+// 		</>
+// 	);
+// };
+// export default Home;
+// const Home = () => {
+
+// 	const[input, setInput] = useState("")
+// 	const[lista, setLista] = useState([])
+
+// 	return (
+// 		<>
+// 		<h3 >{input}</h3>
+// 		<input type="text" onChange={(event)=>setInput(event.target.value)}/>
+// 		<button onClick={()=>setLista([...lista,input])}>Guardar</button>
+// 		<ul>
+// 			{
+// 				lista.map(item=>(<li>{item}</li>))
+// 			}
+// 		</ul>
+// 		</>
+// 	);
+// };
+// export default Home;
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	const [lista, setLista] = useState([])
+
+	const agregarContacto = (contacto) => (
+		setLista([...lista, contacto])
+	)
+	console.log("Lista Completa:", lista);
+	return (
+		<>
+			<Formulario agregarContacto={agregarContacto}></Formulario>
+		</>
 	);
 };
-
 export default Home;
